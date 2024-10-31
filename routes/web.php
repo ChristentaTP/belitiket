a70f938e2b277a17c03eb3e003d05881bc2cd820
ChristentaTP <christentapradieva@gmail.com>
2024-10-31 23:20:06 +0700

    mengubah route agar langsung masuk ke dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+// Route ke dashboard dan profile
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
